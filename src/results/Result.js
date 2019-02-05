9005eb46fc40a014f63cec47f3d3f08b0211fd5b
Nhuuu <nqtrinh87@gmail.com>
2019-02-05 11:33:03 -0800

    result.js and movie lifting

--- src/results/Result.js
+++ src/results/Result.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SERVER_URL from '../constants/server';
 import Weather from './Weather';
 import Music from './Music';
@@ -6,17 +6,44 @@ import Food from './Food';
 import Movie from './Movie';
 
 
+// Need all of the gets to pass down as props for each component?
+class Result extends Component {
+  constructor(){
+    super()
+    this.state = {
+      films:{}
+    }
+  }
 
-// need all of the gets to pass down as props for each component?
+  componentDidMount(){
+    //fetch movie per genre (35 is for comedy)
+    this.getFilms()
+  }
+
+  getFilms = () => {
+     fetch(`https://api.themoviedb.org/3/genre/35/movies?api_key=b1b4d1f42d4ead1ab1d5fb013cb9340d`)
+      .then(response => response.json())
+      .then((json)=> {
+        this.setState({
+          films: json
+        })
+        console.log(this.state.films)
+      })
+      .catch(error => {
+        console.log("Error:", error)
+      })
+    }
+  }
+
+  //getMusic 
+
+  //getFood
 
 
-
-
-
-const Result = () => {
-  return(
-  	<div>
-    	<div>
+  render() {
+    const filmList = this.state.films.map((filmObj, i) => <li key={i}> {filmObj.original_title} </li>)
+    return(
+    	<div className="results">
         <div className="weather-field">
           <Weather />
         </div>
@@ -27,11 +54,12 @@ const Result = () => {
     		  <Food />
         </div>
         <div className="movie-field">
-          <Movie />
+          <Movie films={filmList} />
         </div>
     	</div>
-    </div>
-    )
+    );
+  }
 }
+
 
 export default Result
