305cb6115bc03e2eb4175f35a4f8c6c0c3ecebbf
chidrome <chidrome@gmail.com>
2019-02-05 13:04:38 -0800

    able to edit the  users name and make the call to change it in db

--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
+  
+
+  // Write helper function to capitalize the first letter of the first name of user
+  firstCapitalization = (firstName) => {
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1)
+  }
+
   render() {
     if(this.props.user){
       return (
           <div>
-            <h2>Hello again, {this.props.user.name}!</h2>
+            <h2>Hello again, {this.firstCapitalization(this.props.user.name)}!</h2>
             <h4>Your email is {this.props.user.email}</h4>
             <h4>Your current location is {this.props.user.location}</h4>
-            <button>Edit my Profile</button>
+            <Link to="/profile/edit">Edit Profile</Link>
           </div>
         );
     }
