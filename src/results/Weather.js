4511f945151164dbd373db4b2a54086ff8f95c82
idothestamping <douglas.klemp@gmail.com>
2019-02-06 21:41:46 -0800

    Weather CSS BG change on props

--- src/results/Weather.js
+++ src/results/Weather.js
@@ -4,11 +4,11 @@ import React from 'react'
 const Weather = (props) => {
     return (
       <div className="weather-result">
-        <div className="results-weather-bg"></div>
+        <div className={'results-weather-bg-' + props.cssClass}></div>
         <h3>Current weather is:</h3>
-  
          <div> current weather summary: {props.summary} </div>
          <div> current temperature: {props.temp}℉ </div>
+         <div> current background image from props.icon: {props.cssClass} </div>
 
       </div>
     )
