83a561cf1b3b4928fb3c80be9c545f330f02ba01
hayeun7777 <kellyhypark@gmail.com>
2019-02-07 10:46:36 -0800

    added all images

--- src/results/Restaurant.js
+++ src/results/Restaurant.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Restaurant = (props) => {
-    return(
-    	<div>
-    		<img src={props.poster[2]} alt='' />
-    	</div>
-      );
-  	}
+export default class Restaurant extends Component {
+	render() {
+		let posterList = this.props.poster.map((p, i) =>  { 		
+			return  <div><p key={i}> </p> 
+			<img src={p} alt='' /></div>
+		})
+		return(
+			<div>{posterList}</div>
+		);
+	}
+}
 
-
-export default Restaurant
