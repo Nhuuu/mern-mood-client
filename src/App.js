4e09447edd544e355d68fafabec6f5933f5193e1
Nhuuu <nqtrinh87@gmail.com>
2019-02-09 13:48:54 -0800

    fixed question populating  issue, & kept question/result page from showing when logging out.

--- src/App.js
+++ src/App.js
@@ -18,14 +18,12 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      user: null,
-      currentQuestion: ''
+      user: null
     }
   }
 
   componentDidMount = () => {
     this.getUser()
-    this.getQuestion()
   }
 
   getUser = () => {
@@ -54,24 +52,6 @@ class App extends Component {
     }
   }
 
-  getQuestion = () => {
-    let token = localStorage.getItem('serverToken');
-    axios.post(SERVER_URL + '/question', {
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
-    .then(json => { 
-      const questionArr = json.data[0].questions
-      const questions = questionArr.map((q) => {
-        return q.question
-      })
-        const randQ = questions[Math.floor(questions.length * Math.random())]
-        this.setState({ currentQuestion: randQ })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
   render() {
     return (
       <div>
@@ -95,7 +75,7 @@ class App extends Component {
               () => (<Result user={this.state.user} />)
             } />
             <Route path="/question" component={ 
-              () => (<QuestionForm user={this.state.user} question={this.state.currentQuestion}/>)
+              () => (<QuestionForm user={this.state.user} />)
             } />                       
             <Route path="/profile/edit" component={
               () => (<ProfileEdit user={this.state.user} updateUser={this.getUser} />)
