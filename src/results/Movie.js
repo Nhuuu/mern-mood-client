f72f462067fa162f7c9a76cd58ad0de47c365142
Nhuuu <nqtrinh87@gmail.com>
2019-02-11 13:52:08 -0800

    cleanup

--- src/results/Movie.js
+++ src/results/Movie.js
@@ -2,17 +2,17 @@ import React from 'react'
 import '../App.css'
 
 const Movie = (props) => {
-    return (
-      <div>
-        <div className="results-movie-bg"></div>
-          <h4 className="movie-result-title">Try this movie dude,</h4>
-          <div className="filmPoster">
-            <img className="filmPoster-Img" src={`https://image.tmdb.org/t/p/w500/${props.filmPoster}`} alt="movie poster"/>
-          </div>
-          <h4 className="movie-result"> <b>{props.films}</b> (Vote avg. : {props.filmVote}) </h4>
-          <p className="movie-result"> <span><i>Overview:</i></span> &nbsp; {props.filmOverview}</p>
-      </div>
-    )
+  return (
+    <div>
+      <div className="results-movie-bg"></div>
+        <h4 className="movie-result-title">Try this movie dude,</h4>
+        <div className="filmPoster">
+          <img className="filmPoster-Img" src={`https://image.tmdb.org/t/p/w500/${props.filmPoster}`} alt="movie poster"/>
+        </div>
+        <h4 className="movie-result"> <b>{props.films}</b> (Vote avg. : {props.filmVote}) </h4>
+        <p className="movie-result"> <span><i>Overview:</i></span> &nbsp; {props.filmOverview}</p>
+    </div>
+  )
 }
 
 export default Movie
